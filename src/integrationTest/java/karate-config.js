d5b7968be236169e13e3d1980d1f53cf6fc9f428
vcgdev <vcg_cruz@hotmail.com>
2021-01-24 14:34:24 -0600

    add url configs for karate testing

--- src/integrationTest/java/karate-config.js
+++ src/integrationTest/java/karate-config.js
@@ -1,7 +1,7 @@
 function fn() {
     // run with:  ./gradlew integrationTest "-Purl=http://domain:port/example-demo/app"
     var config = {
-        baseUrl : karate.properties['url'] || 'http://localhost:8082/example-demoss/app'
+        baseUrl : karate.properties['url'] || 'http://localhost:8082/example-demo/app'
     };
 
     return config;
