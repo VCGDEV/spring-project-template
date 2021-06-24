90ede60eb5a412a8c6f8e05174bb72ba07d40f8c
vcgdev <vcg_cruz@hotmail.com>
2021-06-24 05:45:11 -0500

    add oauth2 client for internal calls

--- src/main/java/com/vcgdev/demo/SpringProjectTemplateApplication.java
+++ src/main/java/com/vcgdev/demo/SpringProjectTemplateApplication.java
@@ -6,9 +6,14 @@ import com.vcgdev.demo.config.auth.RestTemplateInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class SpringProjectTemplateApplication {
 
 	private final IAuthenticationFacade authenticationFacade;
@@ -26,6 +31,12 @@ public class SpringProjectTemplateApplication {
 		restTemplate.getInterceptors()
 			.add(new RestTemplateInterceptor(authenticationFacade));
 		return restTemplate;
+	}
+
+	@Bean("oauthRestTemplate")
+	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
+			OAuth2ProtectedResourceDetails details) {
+		return new OAuth2RestTemplate(details, oauth2ClientContext);
 	}
 
 }
